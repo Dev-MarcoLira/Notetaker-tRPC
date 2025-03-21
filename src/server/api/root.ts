abdc1efa6366f2c98eaf8660cce693ecaac57a79
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-03-21 17:19:49 -0300

    Add NoteCard; Add NoteEditor

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { postRouter } from "~/server/api/routers/post";
 import { topicRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -10,16 +11,10 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
-/**
- * Create a server-side caller for the tRPC API.
- * @example
- * const trpc = createCaller(createContext);
- * const res = await trpc.post.all();
- *       ^? Post[]
- */
 export const createCaller = createCallerFactory(appRouter);
